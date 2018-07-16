6a0b077567d598344f3406b4a8bc468da142b5c8
Nick Freear <nfreear@yahoo.co.uk>
2018-07-16 21:50:30 +0100

    Bug #32, add comment-form Javascript handler [ci skip]

---
+++ js/comment.js
@@ -0,0 +1,79 @@
+/*!
+  NDF, 16-July-2018.
+*/
+
+window.jQuery(function ($) {
+
+  const fetch = window.fetch;
+  const $COMMENT_FORM = $('#comment-form');
+  const ACTION = $COMMENT_FORM.attr('action');
+  const $MESSAGE = $COMMENT_FORM.find('.message');
+  // const FIELDS = $COMMENT_FORM.find('input, textarea').filter('[name]');
+
+  $COMMENT_FORM.on('submit', function (ev) {
+    // alert('HI');
+    $MESSAGE.text('Submitting comment ...').addClass('waiting');
+
+    $COMMENT_FORM.find('[ name *= redirect ]').attr('name', 'options[x-redirect]');
+
+    ev.preventDefault();
+
+    console.warn('Comment submit:', ACTION, $COMMENT_FORM.serialize(), params($COMMENT_FORM));
+
+    fetch(ACTION, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        // 'Content-Type': 'application/json; charset=utf-8'
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      redirect: 'manual', // 'follow',
+      body: $COMMENT_FORM.serialize() // JSON.stringify(params($COMMENT_FORM))
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error([ response, response.text() ]); // TODO:
+    })
+    .then(data => {
+      if (data.success) {
+        console.warn('Staticman OK:', data);
+
+        $MESSAGE.text('Thank you! Comment in moderation queue.');
+      } else {
+        console.error('Staticman Error:', data);
+
+        $MESSAGE.text('Error. ' + data.errorCode);
+      }
+    })
+    .catch(error => console.error('Staticman Error (2):', error));
+  });
+
+  console.warn('Comment.js loaded:', $COMMENT_FORM, ACTION, params($COMMENT_FORM));
+
+  // DEPRECATED!
+  function params($form) {
+    var paramObj = {};
+
+    $.each($form.serializeArray(), function(_, kv) {
+      /* if (paramObj.hasOwnProperty(kv.name)) {
+        paramObj[kv.name] = $.makeArray(paramObj[kv.name]);
+        paramObj[kv.name].push(kv.value);
+      }
+      else { */
+        paramObj[ kv.name ] = kv.value;
+      // }
+    });
+
+    return paramObj;
+  }
+
+  // DEPRECATED!
+  function noRedirect(paramObj) {
+    paramObj[ 'options[X-redirect]' ] = paramObj[ 'options[redirect]' ];
+    paramObj[ 'options[redirect]' ] = '';
+
+    return paramObj;
+  }
+});
